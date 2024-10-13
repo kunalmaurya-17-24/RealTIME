04a44e626fd7f499724011f50d356dbeae95891b
kunalmaurya-17-24 <kunalmauryalinkedin@gmail.com>
2024-10-13 13:40:36 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ const app = express();
 const path = require("path");
 
 const http = require("http");
-
 const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine" , "ejs");
-// app.set(express.static(path.join(__dirname,  "public")));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
 
-io.on("connection", function (socket) {
-    socket.on("send-locartion", function (data) {
-        io.emit("recieve-locartion", {id: socket.id, ...data});
-    })
-    socket.on("disconnect", function () {
-        io.emit("user-disconnected", socket.id );
-    })   
-}); 
-
-app.get("/", function (req, res) {
-    res.render("index");    
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
 });
 
-server.listen(3000);
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,40 @@
-const socket = io(); //sent connection request to server
+const socket = io(); // send connection request to server
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position)=>{
-       const  { latitude, longitude } =  position.coords;
-       socket.emit("send-locartion", {latitude, longitude});
-    }, (error)=>{
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
+    }, (error) => {
         console.log(error);
-    },{
+    }, {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0,
-    }
-    );
+    });
 }
 
-const map = L.map("map").setView([0,0], 17); //earth's coordinate and 10 lvl zoom
+const map = L.map("map").setView([0, 0], 17); // Earth's coordinate and zoom level 17
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:"sparkel_god"
-        // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: "sparkel_god"
+    // Correct attribution can be used as needed
 }).addTo(map);
 
 const markers = {};
 
-socket.on("recieve-locartion", (data)=>{
-        const { id, latitude, longitude } =  data;
-        map.setView([latitude, longitude],);
-        if(markers[id]){
-            markers[id].setLatLng([latitude, longitude]);
-        } else {
-            markers[id] = L.marker([latitude, longitude]).addTo(map);
-        }
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
 });
 
-socket.on("user-disconnected", (id)=>{
-    if (markers[id]){
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
-})
+});
